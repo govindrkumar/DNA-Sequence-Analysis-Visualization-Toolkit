e308c8af9499a136b550e39ab287421b64c101b9
govindrkumar <govindkumar123gksingh@gmail.com>
2026-09-02 21:15:59 +0530

    done

--- biseq/settings.py
+++ biseq/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = 'django-insecure-&fltwub_sv7@j!vg1hbk+&+5%&no+k#pbm)+dfn1@bb2jyw!&%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = False
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
